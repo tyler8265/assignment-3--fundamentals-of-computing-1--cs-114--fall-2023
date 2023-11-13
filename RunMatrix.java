a0848d8254b600e8dbf472dee4df3f16b30c16cc
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2023-11-13 01:40:44 -0500

    Summary

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class RunMatrix {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter the size of your matrix: ");
+    int width = scan.nextInt();
+    int height = width;
 
+    System.out.println("Your matrix is")
   }
 }
