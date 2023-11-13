cc7cf568a44b3266683fefde8329bbca16d17003
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2023-11-13 03:16:10 -0500

    idk

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Matrix {
-  
+  int matrix;
+
+  public static void matrix() {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter the size of your matrix: ");
+    int size = scan.nextInt();
+
+    System.out.println("Your matrix is " + size + " x " + size);
+
+    int[][] matrix = new int[size][size];
+  }
+
+
+  public static void printMatrix() {
+    
+
+    for(int i = 0; i < ; i++){
+
+    }
+  }
+
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Please enter the size of your matrix: ");
-    int width = scan.nextInt();
-    int height = width;
+    Matrix runMatrix = new Matrix();
 
-    System.out.println("Your matrix is")
   }
 }
