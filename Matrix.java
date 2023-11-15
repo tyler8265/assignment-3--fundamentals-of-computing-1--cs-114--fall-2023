c9a3a878fb584e29f4658df9a87a8b603ce488e4
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2023-11-15 16:28:04 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -15,13 +15,13 @@ public class Matrix {
     System.out.println("Your matrix is " + size + " x " + size);
     System.out.println();
 
-    matrix = new int[size][size];
+    matrix = new int[size][size]; //Sets the values of the matrix to the user input
   }
 
   private static void swap(int[][] matrix, int x1, int x2, int y1, int y2) {
     if (x1 + y1 == matrix.length - 1 || x2 + y2 == matrix.length - 1){
       return;
-    }
+    } //This if statement deciphers if it is apart of the highlighted indexes to prevent it from swapping if it is
     int tempArray = matrix[x1][y1];
     matrix[x1][y1] = matrix[x2][y2];
     matrix[x2][y2] = tempArray;
@@ -41,7 +41,7 @@ public class Matrix {
       System.out.println();
       for(int j = 0; j < matrix.length; j++){
         if(i + j == matrix.length - 1){
-          System.out.print("\u001b[33m" + matrix[i][j] + "\t\u001B[0m");
+          System.out.print("\u001B[36m" + matrix[i][j] + "\t\u001B[0m");
         }
         else{
           System.out.print(matrix[i][j] + "\t");
@@ -59,6 +59,7 @@ public class Matrix {
 
     rows = 0;
     columns = 0;
+    //DoWhile loop increments the "value" variable which populates the indexes with the correct numbers for the matrix.
     do {
       for (int i = 0; i < matrix.length; i++){
         matrix[rows][columns] = ++value;
@@ -73,10 +74,10 @@ public class Matrix {
       System.out.println();
       for(int j = 0; j < matrix.length; j++){
         if(i + j == matrix.length - 1){
-          System.out.print("\u001b[33m" + matrix[i][j] + "\t\u001B[0m");
+          System.out.print("\u001B[36m" + matrix[i][j] + "\t\u001B[0m"); //Prints the indexes that are diagonal with color codes
         }
         else{
-          System.out.print(matrix[i][j] + "\t");
+          System.out.print(matrix[i][j] + "\t"); //Prints all the other indexes without color codes
         }
       }
     }
@@ -89,6 +90,7 @@ public class Matrix {
     System.out.println("Printing flipped matrix: ");
     rows = 0;
     columns = matrix.length - 1;
+    //This DoWhile loop uses the swap function to swaps
     do {
       for (int i = 0; i < matrix.length; i++){
           swap(matrix, rows, columns, i, matrix.length - 1 - i);
@@ -101,7 +103,7 @@ public class Matrix {
       System.out.println();
       for(int j = 0; j < matrix.length; j++){
         if(i + j == matrix.length - 1){
-          System.out.print("\u001b[33m" + matrix[i][j] + "\t\u001B[0m");
+          System.out.print("\u001B[36m" + matrix[i][j] + "\t\u001B[0m");
         }
         else{
           System.out.print(matrix[i][j] + "\t");
